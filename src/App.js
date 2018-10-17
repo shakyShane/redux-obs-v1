19ea773a0e97e7c8ae4dc457ed099fac5822d660
Shane Osbourne <shane.osbourne8@gmail.com>
2018-10-17 15:20:48 +0100

    5) Fetch data on demand from an API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {connect} from "react-redux";
-import BeerList from "./components/BeersList";
+import Beers from "./components/Beers";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <BeerList />
+                <Beers />
             </div>
         );
     }
--- src/epics/fetchBeers.js
+++ src/epics/fetchBeers.js
@@ -1,10 +1,21 @@
 import {ajax} from "rxjs/ajax";
-import {map} from "rxjs/operators";
-import {fetchFulfilled} from "../reducers/beersActions";
+import {map, switchMap} from "rxjs/operators";
+import {FETCH_DATA, fetchFulfilled, setStatus} from "../reducers/beersActions";
+import {ofType} from "redux-observable";
+import {concat, of} from "rxjs";
+
 const API = 'https://api.punkapi.com/v2/beers';
 
-export function fetchBeersEpic() {
-    return ajax.getJSON(API).pipe(
-        map(resp => fetchFulfilled(resp))
-    )
+export function fetchBeersEpic(action$) {
+    return action$.pipe(
+        ofType(FETCH_DATA),
+        switchMap(() => {
+            return concat(
+                of(setStatus("pending")),
+                ajax.getJSON(API).pipe(
+                    map(resp => fetchFulfilled(resp))
+                )
+            )
+        })
+    );
 }
