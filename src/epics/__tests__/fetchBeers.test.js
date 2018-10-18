0a5a7e9c7f603b4afed6b50c7827d26c86e61ff8
Shane Osbourne <shane.osbourne8@gmail.com>
2018-10-18 19:09:49 +0100

    18) Simulate network errors within tests

--- src/epics/__tests__/fetchBeers.test.js
+++ src/epics/__tests__/fetchBeers.test.js
@@ -1,10 +1,10 @@
 import {TestScheduler} from "rxjs/testing";
-import {fetchFulfilled, search, setStatus} from "../../reducers/beersActions";
+import {cancel, fetchFailed, fetchFulfilled, reset, search, setStatus} from "../../reducers/beersActions";
 import {of} from "rxjs";
 import {initialState} from "../../reducers/configReducer";
 import {fetchBeersEpic} from "../fetchBeers";
 
-it("produces correct actions", function() {
+it("produces correct actions (success)", function() {
     const testScheduler = new TestScheduler((actual, expected) => {
         expect(actual).toEqual(expected);
     });
@@ -30,3 +30,32 @@ it("produces correct actions", function() {
         })
     })
 });
+it("produces correct actions (error)", function() {
+    const testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+    });
+
+    testScheduler.run(({ hot, cold, expectObservable }) => {
+        const action$ = hot('a', {
+            a: search("ship")
+        });
+        const state$ = of({
+            config: initialState
+        });
+        const dependencies = {
+            getJSON: (url) => {
+                return cold('-#', null, {
+                    response: {
+                        message: "oops!"
+                    }
+                })
+            }
+        };
+        const output$ = fetchBeersEpic(action$, state$, dependencies);
+        expectObservable(output$).toBe('500ms ab', {
+            a: setStatus("pending"),
+            b: fetchFailed("oops!")
+        })
+    })
+});
+
